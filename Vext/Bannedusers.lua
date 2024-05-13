f55a5950a21d6302a5ee99476abbba0ba835f6e6
Iamiakk <153729760+Iamiak@users.noreply.github.com>
2024-05-13 18:11:34 +0530

    Update Bannedusers.lua

--- Vext/Bannedusers.lua
+++ Vext/Bannedusers.lua
@@ -2,3 +2,9 @@ local bannedusers = {
   "5834886008", 
   "1111111"
 }
+
+if game.Players.LocalPlayer.UserId == bannedusers then
+  statusLabel.Text = "You have been banned. Join discord for support. Kick in 30 secs."
+  wait(30)
+  game.Players.LocalPlayer:Kick("You have been banned. Join discord for support.")
+end
